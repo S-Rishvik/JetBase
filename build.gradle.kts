d83988bda49a65fa30daa592737868fe605f6186
sangaraboinarishvik <rishvik.vardhan@navi.com>
2023-10-02 20:43:45 +0530

    Added spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,20 @@ buildscript {
     mavenCentral()
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:8.1.0")
+    classpath("com.android.tools.build:gradle:8.1.2")
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
-
+    classpath("com.diffplug.spotless:spotless-plugin-gradle:6.20.0")
     // NOTE: Do not place your application dependencies here; they belong
     // in the individual module build.gradle files
+  }
+}
+
+plugins {
+  id("com.diffplug.spotless") version "6.20.0" apply false
+}
+
+subprojects {
+  afterEvaluate {
+    project.apply("../spotless.gradle")
   }
 }
