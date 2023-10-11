d2e2de7f1407eddc9393ba56383948882f3cc601
sangaraboinarishvik <rishvik.vardhan@navi.com>
2023-10-11 20:52:21 +0530

    Refactoring

--- data/src/main/java/com/example/data/remote/github/GithubApiService.kt
+++ data/src/main/java/com/example/data/remote/github/GithubApiService.kt
@@ -1,35 +1,15 @@
 package com.example.data.remote.github
 
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface GithubApiService {
     companion object {
-        private const val BASE_URL = "https://api.github.com/"
         private const val PATH_OWNER = "owner"
         private const val PATH_REPO = "repo"
         private const val Query_STATE = "state"
-        private const val END_POINT_GET_PULL_REQUEST = "repos/{$PATH_OWNER}/{$PATH_REPO}/pulls"
-
-        fun getRetrofitService(): GithubApiService {
-            val okHttpLoggingInterceptor by lazy {
-                HttpLoggingInterceptor().apply {
-                    level = HttpLoggingInterceptor.Level.BODY
-                }
-            }
-            val retrofit =
-                Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .client(OkHttpClient.Builder().addInterceptor(okHttpLoggingInterceptor).build())
-                    .build()
-            return retrofit.create(GithubApiService::class.java)
-        }
+        private const val END_POINT_GET_PULL_REQUEST = "{$PATH_REPO}/pulls"
     }
 
     @GET(END_POINT_GET_PULL_REQUEST)
